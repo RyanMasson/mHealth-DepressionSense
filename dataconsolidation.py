f4ca4ae273e2fc4ad4cbacdca1d70cddd6b84d73
Sophia <sophialou2017@u.northwestern.edu>
2017-02-15 22:22:22 -0600

    added error checking for missing files

--- dataconsolidation.py
+++ dataconsolidation.py
@@ -10,6 +10,8 @@ import numpy as np
 
 def csvToDataFrame(path_to_file):
     csvData = []
+    if not os.path.exists(path_to_file):
+        return None
     data = open(path_to_file).readlines()
     for row in data:
         row = [x.rstrip() for x in row.split('\t')]
@@ -20,7 +22,10 @@ def csvToDataFrame(path_to_file):
 def getDirNames(path_to_directory):
     names = os.listdir(path_to_directory)
     names.pop(0)
-    return names
+    badfile = names.index("desktop.ini")
+
+    # del names[badfile]
+    return names[badfile+1:]    # to finish running wifi
 
 def getFileNames(path_to_directory):
     names = [f for f in listdir(path_to_directory) if isfile(join(path_to_directory, f)) and 'DS_Store' not in f and 'Sohrob' not in f]
@@ -33,6 +38,8 @@ def isSameDay(timestamp1, timestamp2):
 def labelEachDay(subjectID):
     sleepFile = '../CS120/' + subjectID + '/ems.csv'
     sleepReport = csvToDataFrame(sleepFile)
+    if sleepReport is None:
+        return None
     sleepReport = sleepReport.drop(sleepReport.columns[1:6], axis=1)
     sleepReport.columns = ['timestamp','label']
     return sleepReport
@@ -41,46 +48,48 @@ def addWifiMean(subjectID,df):
     wifiFile = '../CS120/' + subjectID + '/wif.csv'
     wifiReport = csvToDataFrame(wifiFile)
 
-    days = len(df)
+    if wifiReport is not None:
+        days = len(df)
 
-    wifiPoints = np.full((days,1),0)
-    wifiCounts = np.full((days,1),0)
+        wifiPoints = np.full((days,1),0)
+        wifiCounts = np.full((days,1),0)
 
-    # goes through the wifi data frame
-    for i in range(len(wifiReport)):
-        ts = wifiReport[0][i]
-        accessPoints = float(wifiReport[3][i])
+        # goes through the wifi data frame
+        for i in range(len(wifiReport)):
+            ts = wifiReport[0][i]
+            accessPoints = float(wifiReport[3][i])
 
-        # find the same day within BigFile
-        for index,row in df.iterrows():
-            day_ts = row['timestamp']
-            day = int(row['day']) - 1
-            if isSameDay(ts,day_ts):
-                wifiPoints[day][0] += accessPoints
-                wifiCounts[day][0] += 1
-                break
+            # find the same day within BigFile
+            for index,row in df.iterrows():
+                day_ts = row['timestamp']
+                day = int(row['day']) - 1
+                if isSameDay(ts,day_ts):
+                    wifiPoints[day][0] += accessPoints
+                    wifiCounts[day][0] += 1
+                    break
 
-    for i in range(days):
-        point = wifiPoints[i][0]
-        count = wifiCounts[i][0]
-        wifiPoints[i][0] = (point / count) if (count > 0) else 0
+        for i in range(days):
+            point = wifiPoints[i][0]
+            count = wifiCounts[i][0]
+            wifiPoints[i][0] = (point / count) if (count > 0) else 0
 
-    df['wifi_mean'] = wifiPoints
+        df['wifi_mean'] = wifiPoints
     return df
 
 def createBigCSV(path_to_directory):
     participantIDs = getDirNames(path_to_directory)
     for Id in participantIDs:
         labels = labelEachDay(Id)
-        labels['day'] = np.arange(1,len(labels)+1)
-        labels['id'] = Id;
+        if labels is not None:
+            labels['day'] = np.arange(1,len(labels)+1)
+            labels['id'] = Id;
 
-        df = addWifiMean(Id,labels)
+            df = addWifiMean(Id,labels)
 
-        if (os.path.exists("bigFile.csv")):
-            labels.to_csv('bigFile.csv', mode='a', index=False, header=False)
-        else:
-            labels.to_csv('bigFile.csv', index=False, header=True)
+            if (os.path.exists("bigFile.csv")):
+                labels.to_csv('bigFile.csv', mode='a', index=False, header=False)
+            else:
+                labels.to_csv('bigFile.csv', index=False, header=True)
 
 def addFeature2csv(feature_name,nparray_values):
     csv_input = pd.read_csv('bigFile.csv')
@@ -92,3 +101,4 @@ def dropFeature(feature_index):
     csv_input = pd.read_csv('bigFile.csv')
     csv_input = csv_input.drop(csv_input.columns[feature_index:feature_index + 1], axis=1)
     csv_input.to_csv('bigFile.csv', index=False, header=True)
+
